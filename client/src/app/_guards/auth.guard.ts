84b4868190363c519674107466181563d8bda8a4
aditya14082000 <aditya2000nair@gmail.com>
2022-11-02 14:38:37 +0530

    End_of_section_8

--- client/src/app/_guards/auth.guard.ts
+++ client/src/app/_guards/auth.guard.ts
@@ -15,8 +15,10 @@ export class AuthGuard implements CanActivate {
       map(user => {
         if (user) return true;
         this.toastr.error('YOu shall not pass!')
+        return false;
       })
-    );
+    
+    )
   }
   
 }
